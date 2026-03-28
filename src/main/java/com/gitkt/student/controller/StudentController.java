02a1892c843a0c002d599d789272248db903edc1
Vinit538 <vinitkaratagi538@gmail.com>
2026-03-28 09:46:21 +0530

    Updated Changed comment in conflict-demo branch

--- src/main/java/com/gitkt/student/controller/StudentController.java
+++ src/main/java/com/gitkt/student/controller/StudentController.java
@@ -21,7 +21,7 @@ public class StudentController {
     public List<Student> getAllStudents(
             @RequestParam(required = false) String course,
             @RequestParam(required = false) String grade) {
-        // fetching all students
+        // fetching all students new test
         if (course != null) return studentService.getStudentsByCourse(course.trim());
         if (grade != null) return studentService.getStudentsByGrade(grade.trim());
         return studentService.getAllStudents();
